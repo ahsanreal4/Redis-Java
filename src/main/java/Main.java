b1d118668703025317857182e624b40ed9dae5bc
ahsanreal4 <ahsan.btph123@gmail.com>
2024-10-04 14:32:11 +0500

    send response to client twice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,16 +10,17 @@ public class Main {
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-          clientSocket = serverSocket.accept();
+            clientSocket = serverSocket.accept();
 
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-          clientSocket.getInputStream().read();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String line = reader.readLine();
+            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
 
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
